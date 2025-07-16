03dde2af84b2e05cf4b80030c68970bd0aa0bc54
z3sh4n <ghaniwalamohmmad@gmail.com>
2025-07-16 18:05:37 +0530

    added export defaults in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,15 @@
+// sitemap.ts
+import { MetadataRoute } from 'next'; // Import MetadataRoute type from 'next'
+
 export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://zeesshhh.vercel.app';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: baseUrl, // Use your baseUrl here
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+  ];
+}
